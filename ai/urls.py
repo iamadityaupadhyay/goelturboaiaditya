6db558b81adc0102956dcb808b9b5e6be827cac2
Aditya Upadhyay <uaditya219@gmail.com>
2024-11-22 15:27:20 +0530

    new

--- ai/urls.py
+++ ai/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/', views.predict_influence, name='predict_influence'),
+    path('', views.predict_influence, name='predict_influence'),
 ]
--- goelturboai/urls.py
+++ goelturboai/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('ap/', include('ai.urls'))
+     path('', include('ai.urls'))
 ]
